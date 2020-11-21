0053bce2439c2ce2971df0aa9d560a27389165d2
Vyacheslav.Mukhin <Vyacheslav.Mukhin@x5.ru>
2020-11-21 18:18:06 +0300

    Exercise1 is done

---
+++ chapter1/Exercise1.java
@@ -0,0 +1,13 @@
+//1. Создайте класс с полями int и char, которые не инициализируются в программе.
+//   Выведите их значения, чтобы убедиться в том, что Java выполняет инициализацию
+//   по умолчанию.
+
+public class Exercise1 {
+  static int Int;
+  static char Char;
+
+  public static void main(String[] args) {
+    System.out.println(Int);
+    System.out.println(Char);
+  }
+}
